dabe32614e1ba8d6280d2fa62fd44b92edc2aca3
AndreyG75 <74177588+AndreyG75@users.noreply.github.com>
2021-08-15 16:17:37 +0300

    NPM Scripts Part 1

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+<script>
+$("#carouselButton").click(function(){
+    if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+        $("#mycarousel").carousel('pause');
+        $("#carouselButton").children("span").removeClass('fa-pause');
+        $("#carouselButton").children("span").addClass('fa-play');
+    }
+    else if ($("#carouselButton").children("span").hasClass('fa-play')){
+        $("#mycarousel").carousel('cycle');
+        $("#carouselButton").children("span").removeClass('fa-play');
+        $("#carouselButton").children("span").addClass('fa-pause');
+    }
+});
+
+$("#ButtonLogin").click(function(){
+$("#loginModal").modal('show');
+});
+
+$("#ButtonReserve").click(function(){
+$("#ReserveModal").modal('show');
+});
+
+</script>
